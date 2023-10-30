2f03ae376f9a5fabbbcd9c52dc5d8700d65f5826
myominlatt <myominlatt330@gmail.com>
2023-10-30 19:34:52 +0630

    port 3001

--- bin/server.dart
+++ bin/server.dart
@@ -27,7 +27,7 @@ void main(List<String> args) async {
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '3001');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }
